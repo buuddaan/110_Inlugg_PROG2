1ebf5ab67d24b1cc242851006af4c31fc98b55ce
Elvira <elvirale3i@gmail.com>
2025-05-06 17:22:34 +0200

    innan avslut för dagen, lagt till funktion för image + fönster

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -4,17 +4,25 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+
 
 public class Gui extends Application {
 
+  StackPane pane = new StackPane(); //Instansvariabel för att kunna återanvändas
+  ImageView imageView = new ImageView(); //Läggs innan start() för att kunna nås av alla metoder
+
+
+
   public void start(Stage primaryStage) {
     Graph<String> graph = new ListGraph<String>();
-
     BorderPane root = new BorderPane();
+    pane.setStyle("-fx-background-color: lightgray;"); //Bara för att visa vart Pane ligger innan bild kommer
 
     MenuBar menuBar = new MenuBar();
     Menu fileMenu = new Menu("File");
@@ -42,17 +50,43 @@ public class Gui extends Application {
 
     VBox holdTop = new VBox();
     holdTop.getChildren().addAll(menuBar, hbox);
-    
+
     root.setTop(holdTop); //OBS: Måste ligga innan vi visar scenen för att funka
+    root.setCenter(pane);
 
-    Scene scene = new Scene(root); //640, 480 tidigare storlek
+    newMap.setOnAction(event -> {
+      FileChooser fileChooser = new FileChooser();
+      fileChooser.setTitle("Choose a map image");
+      fileChooser.getExtensionFilters().addAll(
+              new FileChooser.ExtensionFilter("Image files", "*.png", "*.jpg", "*.jpeg", "*.gif")
+      );
+
+
+      File selectedFile = fileChooser.showOpenDialog(primaryStage); //showOpenD.. pop-up "Open File"
+      if (selectedFile != null) {
+        javafx.scene.image.Image image = new javafx.scene.image.Image(selectedFile.toURI().toString());
+        imageView.setImage(image);
+        imageView.setPreserveRatio(true);
+        imageView.setFitWidth(image.getWidth());
+        imageView.setFitHeight(image.getHeight());
+
+        pane.getChildren().clear();
+        pane.getChildren().add(imageView);
+        pane.setPrefSize(image.getWidth(), image.getHeight());
+        primaryStage.setWidth(image.getWidth()); //testar
+        primaryStage.setHeight(image.getHeight()); //testar
+
+      }
+    });
+
+    Scene scene = new Scene(root, 600, 400);
     primaryStage.setScene(scene);
-    primaryStage.setTitle("PathFinder");
+    primaryStage.setTitle("PathFinder"); //Stilgrej att den är till vänster i Windows/Linux men i mitten för Mac
     primaryStage.show();
-
   }
 
-  public static void main(String[] args) {
-    launch(args);
-  }
+  public static void main(String[] args){
+      launch(args);
+    }
 }
+
